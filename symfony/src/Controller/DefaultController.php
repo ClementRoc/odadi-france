8da9adeb049dfc4bd9b6787be2f9b7380c258cdd
Clément <clem.roc5@gmail.com>
2023-10-19 11:47:24 +0200

    started the website structure

--- symfony/src/Controller/DefaultController.php
+++ symfony/src/Controller/DefaultController.php
@@ -18,4 +18,36 @@ class DefaultController extends AbstractController
     {
         return $this->render('home.html.twig');
     }
+
+    /**
+     * @Route("/actualités", name="app_news", methods="GET")
+     */
+    public function actuality(): Response
+    {
+        return $this->render('news.html.twig');
+    }
+
+    /**
+     * @Route("/nos-actions", name="app_actions", methods="GET")
+     */
+    public function actions(): Response
+    {
+        return $this->render('actions.html.twig');
+    }
+
+    /**
+     * @Route("/histoire", name="app_history", methods="GET")
+     */
+    public function history(): Response
+    {
+        return $this->render('history.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="app_contact", methods="GET")
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
 }
