098aba719218bcfb3282839c108f0124b21070f5
Clément Rocolle <clement.roc@outlook.fr>
2024-03-12 17:33:58 +0100

    abord contact form -> 2.0 started RGPD integration

---
+++ symfony/src/Controller/RGPDController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\NewsService;
+use App\Service\MembersService;
+use App\Service\MailerService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RGPDController extends AbstractController
+{
+    /**
+     * @Route("/données-personnelles", name="app_data", methods="GET")
+     */
+    public function data(): Response
+    {
+        return $this->render('rgpd/data.html.twig');
+    }
+
+    /**
+     * @Route("/mentions-légales", name="app_legal", methods="GET")
+     */
+    public function legal(): Response
+    {
+        return $this->render('rgpd/legal.html.twig');
+    }
+}
